e70f3a07216dda3c73c03e347197fdcd75ddbe3d
Andrew Clayton <andrew@digital-domain.net>
2021-04-16 18:08:23 +0100

    itsa: Add extra colour output infrastructure
    While we currently have colourised output, there is no way to turn it
    off.
    
    This adds the main infrastructure that will allow that.
    
    We could have just made the escape codes variables (that could then be
    set to "") but then they couldn't be used as preprocessor string
    literals and would have to have gone in as %s's in the format strings
    would I don't think would have been the best from a readability
    standpoint.
    
    So the way this works now is with a simple parser that parses tags in a
    format string that says how to colourise the text.
    
    E.g to write 'Hello World' in blue
    
        printc("#BLUE#Hello World#RST#\n");
    
    The tag name is between the #'s and the #RST# is for 'reset'.
    
    printc() operates just like printf(3) and takes the same format strings.
    
    There are also several helper functions (well, technically they are
    macro wrappers around a generic function) for doing some common
    printing, such as the error messages.
    
    printic() - for INFO messages
    printsC() - for OK messages
    printcc() - for CONFIRM messages
    printwc() - for WARNING messages
    printec() - for ERROR messages
    
    Colourised output can be disabled by setting the NO_COLOR environment
    variable, its value (if any) is unimportant, which simply causes the
    tags to be removed from the format string.
    
    This follows the informal standard at https://no-color.org/
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

---
+++ src/color.c
@@ -0,0 +1,223 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * color.c - colourised output
+ *
+ * Copyright (c) 2021		Andrew Clayton <andrew@digital-domain.net>
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "color.h"
+
+#define MAX_COLOR_NAME		32
+
+static const struct color {
+	const char *color;
+	const char *code;
+} colors[] = {
+	{ "HI_YELLOW",		TC_HI_YELLOW		},
+	{ "HI_GREEN",		TC_HI_GREEN		},
+	{ "HI_RED",		TC_HI_RED		},
+	{ "HI_BLUE",		TC_HI_BLUE		},
+	{ "GREEN",		TC_GREEN		},
+	{ "RED",		TC_RED			},
+	{ "BLUE",		TC_BLUE			},
+	{ "CHARC",		TC_CHARC		},
+	{ "TANG",		TC_TANG			},
+
+	{ "BOLD",		TC_BOLD			},
+	{ "RST",		TC_RST			},
+
+	{ "MSG_INFO",		TC_HI_BLUE		},
+	{ "MSG_WARN",		TC_HI_YELLOW		},
+	{ "MSG_ERR",		TC_HI_RED		},
+
+	{ "INFO",		TC_BLUE			},
+	{ "CONFIRM",		TC_CHARC		},
+	{ "WARNING",		TC_HI_YELLOW		},
+	{ "SUCCESS",		TC_HI_GREEN		},
+	{ "ERROR",		TC_HI_RED		},
+
+	{ "STRUE",		TC_HI_GREEN		},
+	{ "SFALSE",		TC_HI_RED		},
+
+	{}
+};
+
+static const char *lookup(const char *color)
+{
+	const struct color *ptr = colors;
+
+	for ( ; ptr->color != NULL; ptr++) {
+		if (strcmp(color, ptr->color) == 0)
+			return ptr->code;
+	}
+
+	return NULL;
+}
+
+#define ALLOC_SZ	64
+static void *srealloc(char **base, size_t extra, char **ptr, size_t *alloc,
+		      char **sptr)
+{
+	size_t len = *ptr - *base;
+	size_t slen = *sptr - *base;
+
+	if (len + extra < *alloc)
+		return *base;
+
+	*base = realloc(*base, *alloc + ALLOC_SZ);
+	*alloc += ALLOC_SZ;
+
+	*ptr = *base + len;
+	*sptr = *base + slen;
+
+	return *base;
+}
+
+static char *parser(const char *buf)
+{
+	char *new = malloc(ALLOC_SZ);
+	char *ptr = new;
+	char *sptr = new;
+	char color[MAX_COLOR_NAME];
+	char *cptr = color;
+	bool in_color = false;
+	size_t alloc = ALLOC_SZ;
+
+	while (*buf) {
+		const char *code = "\0";
+
+		*ptr = *buf;
+
+		if (*buf == '#' && !in_color) {
+			sptr = ptr;
+			cptr = color;
+			in_color = true;
+			goto next;
+		} else if (in_color && *buf != '#') {
+			*cptr = *buf;
+			cptr++;
+			goto next;
+		} else if (!in_color) {
+			goto next;
+		}
+
+		/*
+		 * in_color == true and we have a trailing '#', see if
+		 * it's a colour code.
+		 */
+		in_color = false;
+		*cptr = '\0';
+		if (!getenv("NO_COLOR"))
+			code = lookup(color);
+
+		if (code && *code == '\0') {
+			ptr = sptr - 1;
+		} else if (code) {
+			size_t clen = strlen(code);
+
+			ptr = sptr;
+			new = srealloc(&new, clen, &ptr, &alloc, &sptr);
+			memcpy(ptr, code, clen);
+			ptr += clen - 1;
+		} else {
+			/*
+			 * Handle cases like
+			 *
+			 *   #XXXX##text...
+			 *
+			 * Note the extra '#' which should appear in the
+			 * output string.
+			 */
+			buf--;
+			ptr--;
+		}
+
+next:
+		new = srealloc(&new, 1, &ptr, &alloc, &sptr);
+		buf++;
+		ptr++;
+	}
+	*ptr = '\0';
+
+	return new;
+}
+
+void printc_xtra(FILE *fp, enum msg_type type, const char *fmt, ...)
+{
+	va_list ap;
+	char *buf = NULL;
+	char *cstr;
+	char *str;
+	const char *tstr;
+	int len;
+
+	switch (type) {
+	case MT_ERROR:
+		tstr = "[#ERROR#ERROR#RST#] ";
+		break;
+	case MT_WARNING:
+		tstr = "[#WARNING#WARNING#RST#] ";
+		break;
+	case MT_INFO:
+		tstr = "[#INFO#INFO#RST#] ";
+		break;
+	case MT_CONFIRM:
+		tstr = "[#CONFIRM#CONFIRMATION#RST#] ";
+		break;
+	case MT_SUCCESS:
+		tstr = "[#SUCCESS#OK#RST#] ";
+		break;
+	default:
+		tstr = "";
+	}
+
+	str = malloc(strlen(fmt) + strlen(tstr) + 1);
+	memcpy(str, tstr, strlen(tstr));
+	memcpy(str + strlen(tstr), fmt, strlen(fmt) + 1);
+
+	va_start(ap, fmt);
+	len = vasprintf(&buf, str, ap);
+	va_end(ap);
+	if (len == -1)
+		goto out_free;
+
+	cstr = parser(buf);
+	fprintf(fp, "%s", cstr);
+
+	free(cstr);
+
+out_free:
+	free(str);
+	free(buf);
+}
+
+void printc(const char *fmt, ...)
+{
+	va_list ap;
+	char *buf = NULL;
+	char *cstr;
+	int len;
+
+	va_start(ap, fmt);
+	len = vasprintf(&buf, fmt, ap);
+	va_end(ap);
+	if (len == -1)
+		goto out_free;
+
+	cstr = parser(buf);
+	printf("%s", cstr);
+
+	free(cstr);
+
+out_free:
+	free(buf);
+}
--- src/color.h
+++ src/color.h
@@ -9,6 +9,8 @@
 #ifndef _COLOR_H_
 #define _COLOR_H_
 
+#include <stdio.h>
+
 #define TC_HI_YELLOW		"\e[38;5;11m"
 #define TC_HI_GREEN		"\e[38;5;10m"
 #define TC_HI_RED		"\e[38;5;9m"
@@ -21,5 +23,26 @@
 
 #define TC_BOLD			"\e[1m"
 #define TC_RST			"\e[0m"
+
+enum msg_type {
+	MT_ERROR,
+	MT_WARNING,
+	MT_INFO,
+	MT_CONFIRM,
+	MT_SUCCESS,
+};
+
+#define __fmt_printf(n, m)	__attribute__((format(printf, n, m)))
+/* forward declaration */
+void printc_xtra(FILE *fp, enum msg_type type, const char *fmt, ...)
+	__fmt_printf(3, 4);
+
+#define printec(fmt, ...) printc_xtra(stderr, MT_ERROR, fmt, ##__VA_ARGS__)
+#define printwc(fmt, ...) printc_xtra(stdout, MT_WARNING, fmt, ##__VA_ARGS__)
+#define printcc(fmt, ...) printc_xtra(stdout, MT_CONFIRM, fmt, ##__VA_ARGS__)
+#define printsc(fmt, ...) printc_xtra(stdout, MT_SUCCESS, fmt, ##__VA_ARGS__)
+#define printic(fmt, ...) printc_xtra(stdout, MT_INFO, fmt, ##__VA_ARGS__)
+
+extern void printc(const char *fmt, ...) __fmt_printf(1, 2);
 
 #endif /* _COLOR_H_ */
