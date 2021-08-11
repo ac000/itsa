ac000/itsa

commit 498b3226f3e9be0d38177665946b4d6f4b75d482

itsa: Update for API changes
Recently various endpoints have been moved around/changed and removed[0]

Also the format of some of the endpoints has changed in what arguments
they take and the format of the output JSON.

This basically does the necessary to get things working how they were,
sometimes it was just a change in function name and parameters, some
needed more work.

[0]: https://github.com/ac000/libmtdac/commit/215cffab2808caea0c27ada5af0437665051c77f

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/itsa.c
@@ -27,9 +27,11 @@
 #include <jansson.h>
 
 #include <libmtdac/mtd.h>
-#include <libmtdac/mtd-sa.h>
-#include <libmtdac/mtd-ic.h>
+#include <libmtdac/mtd-bd.h>
 #include <libmtdac/mtd-biss.h>
+#include <libmtdac/mtd-ic.h>
+#include <libmtdac/mtd-ob.h>
+#include <libmtdac/mtd-sa.h>
 
 #include <libac.h>
 
@@ -967,12 +969,13 @@ static int get_eop_obligations(void)
 	json_t *result;
 	json_t *obs;
 	json_t *period;
+	const char *qs = "?typeOfBusiness=self-employment";
 	char *jbuf;
 	size_t index;
 	int ret = -1;
 	int err;
 
-	err = mtd_sa_se_get_end_of_period_obligations(SEID, NULL, &jbuf);
+	err = mtd_ob_list_end_of_period_obligations(qs, &jbuf);
 	if (err) {
 		printec("Couldn't get End of Period Statement(s). (%s)\n%s\n",
 			mtd_err2str(err), jbuf);
@@ -1617,7 +1620,7 @@ static void get_period(char **start, char **end)
 	char *jbuf;
 	int err;
 
-	err = mtd_sa_se_list_obligations(SEID, NULL, &jbuf);
+	err = mtd_ob_list_inc_and_expend_obligations(NULL, &jbuf);
 	if (err) {
 		printec("Couldn't get list of obligations. (%s)\n%s\n",
 			mtd_err2str(err), jbuf);
@@ -1674,7 +1677,7 @@ static int list_periods(int argc, char *argv[])
 	json_t *result;
 	json_t *obs;
 	json_t *period;
-	char qs[32] = "\0";
+	char qs[128] = "?typeOfBusiness=self-employment";
 	int err;
 	size_t index;
 	char *jbuf;
@@ -1685,13 +1688,14 @@ static int list_periods(int argc, char *argv[])
 	}
 
 	if (argc > 2) {
-		int len;
+		int len = strlen(qs);
 
-		len = snprintf(qs, sizeof(qs), "?from=%s", argv[2]);
-		snprintf(qs + len, sizeof(qs) - len, "&to=%s", argv[3]);
+		len += snprintf(qs + len, sizeof(qs) - len, "&fromDate=%s",
+				argv[2]);
+		snprintf(qs + len, sizeof(qs) - len, "&toDate=%s", argv[3]);
 	}
 
-	err = mtd_sa_se_list_obligations(SEID, qs, &jbuf);
+	err = mtd_ob_list_inc_and_expend_obligations(qs, &jbuf);
 	if (err) {
 		printec("Couldn't get list of obligations. (%s)\n%s\n",
 			mtd_err2str(err), jbuf);
@@ -1701,6 +1705,10 @@ static int list_periods(int argc, char *argv[])
 
 	result = get_result_json(jbuf);
 	obs = json_object_get(result, "obligations");
+	if (!obs)
+		goto out_free;
+	obs = json_array_get(obs, 0);
+	obs = json_object_get(obs, "obligationDetails");
 
 	printc("#CHARC#  %14s %18s %11s %12s %8s#RST#\n",
 	       "period_id", "start", "end", "due", "met" );
@@ -1708,21 +1716,22 @@ static int list_periods(int argc, char *argv[])
 	       " ------------------------------------------------------------"
 	       "---------#RST#\n");
 	json_array_foreach(obs, index, period) {
-		json_t *start_obj = json_object_get(period, "start");
-		json_t *end_obj = json_object_get(period, "end");
-		json_t *due_obj = json_object_get(period, "due");
-		json_t *met = json_object_get(period, "met");
+		json_t *start_obj = json_object_get(period, "periodStartDate");
+		json_t *end_obj = json_object_get(period, "periodEndDate");
+		json_t *due_obj = json_object_get(period, "dueDate");
+		json_t *rec_obj = json_object_get(period, "receivedDate");
 		const char *start = json_string_value(start_obj);
 		const char *end = json_string_value(end_obj);
 		const char *due = json_string_value(due_obj);
 
 		printc("%s  %s_%-14s %-12s %-12s %-12s%s %s\n",
-		       get_period_color(start, end, due, json_is_true(met)),
+		       get_period_color(start, end, due,
+					rec_obj ? true : false),
 		       start, end, start, end, due,
-		       "#RST#",
-		       json_is_true(met) ? STRUE : SFALSE);
+		       "#RST#", rec_obj ? STRUE : SFALSE);
         }
 
+out_free:
 	json_decref(result);
 	free(jbuf);
 
@@ -2077,7 +2086,7 @@ static int do_init_all(const struct mtd_cfg *cfg)
 		return err;
 
 	printic("Looking up SEID...\n");
-	err = mtd_sa_se_list_employments(&jbuf);
+	err = mtd_bd_list(&jbuf);
 	if (err) {
 		printec("Couldn't get list of employments. (%s)\n%s\n",
 			mtd_err2str(err), jbuf);
@@ -2085,8 +2094,9 @@ static int do_init_all(const struct mtd_cfg *cfg)
 	}
 
 	result = get_result_json(jbuf);
-	employment = json_array_get(result, json_array_size(result) - 1);
-	seid = json_object_get(employment, "id");
+	employment = json_object_get(result, "listOfBusinesses");
+	employment = json_array_get(employment, 0);
+	seid = json_object_get(employment, "businessId");
 	if (!seid) {
 		printec("No employments found\n%s\n", jbuf);
 		goto out_free;

commit 34bf36dade8c61262ee663890d3813e9df64b5e0

itsa: Fix a crash with create-period
When trying to create a periodic update we were getting a segfault.

This was introduced by commit 498b322 ("itsa: Update for API changes")
which changed get_period() to use
mtd_ob_list_inc_and_expend_obligations() instead of
mtd_sa_se_list_obligations() to get the list of periods.

Unfortunately that isn't enough, as the format of the returned JSON has
totally changed which meant were trying to strdup(3) a NULL pointer.

We basically need to do the same changes we _did_ make to list_period().

This still doesn't actually work but that seems more due to the HMRC MTD
testing environment[0]:

[0]: https://github.com/hmrc/income-tax-mtd-changelog/issues/3

Fixes: 498b322 ("itsa: Update for API changes")
Closes: https://github.com/ac000/itsa/issues/2
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/itsa.c
@@ -1630,37 +1630,44 @@ static void get_period(char **start, char **end)
 	json_t *obs;
 	json_t *period;
 	size_t index;
+	const char *qs = "?typeOfBusiness=self-employment";
 	char *jbuf;
 	int err;
 
-	err = mtd_ob_list_inc_and_expend_obligations(NULL, &jbuf);
+	err = mtd_ob_list_inc_and_expend_obligations(qs, &jbuf);
 	if (err) {
 		printec("Couldn't get list of obligations. (%s)\n%s\n",
 			mtd_err2str(err), jbuf);
-		goto out_free;
+		goto out_free_jbuf;
 	}
 
 	result = get_result_json(jbuf);
 	obs = json_object_get(result, "obligations");
+	if (!obs)
+		goto out_free_json;
+	obs = json_array_get(obs, 0);
+	obs = json_object_get(obs, "obligationDetails");
 	json_array_foreach(obs, index, period) {
-                json_t *met;
+		json_t *status;
 		json_t *start_obj;
 		json_t *end_obj;
 
-		met = json_object_get(period, "met");
-		if (json_is_true(met))
+		status = json_object_get(period, "status");
+		if (strcmp(json_string_value(status), "Fulfilled") == 0)
 			continue;
 
-		start_obj = json_object_get(period, "start");
-		end_obj = json_object_get(period, "end");
+		start_obj = json_object_get(period, "periodStartDate");
+		end_obj = json_object_get(period, "periodEndDate");
 		*start = strdup(json_string_value(start_obj));
 		*end = strdup(json_string_value(end_obj));
 
 		break;
         }
+
+out_free_json:
 	json_decref(result);
 
-out_free:
+out_free_jbuf:
 	free(jbuf);
 }
 